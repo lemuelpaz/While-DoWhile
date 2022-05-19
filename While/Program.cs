4bb572468a7953e272c3c1b4b5ab7c4f7ef15cff
lemuelpaz <lemuelpazcontato@hotmail.com>
2022-05-18 22:16:33 -0300

    while-dowhile

---
+++ While/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ////int i = 0;
+            ////while(i < 5)
+            ////{
+            ////    Console.WriteLine(i);
+            ////    i++;
+            ////}
+
+            int i = 0;
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            while (i < 5);
+
+        }
+    }
+}
